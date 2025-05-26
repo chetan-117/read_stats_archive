00e32137a22b1c48f0c757f0ee6f63e6e2c957f8
srm <chetantripathi31@gmail.com>
2025-05-26 09:36:59 +0530

    feature: detecting installed packages (dpkg, rpm, snap, flatpaks)

--- utilities.py
+++ utilities.py
@@ -1,4 +1,6 @@
-# import os
+import os
+import sqlite3
+
 # import subprocess
 
 
@@ -40,3 +42,67 @@ def get_uptime_seconds() -> float:
 def get_shell() -> str:
     return os.environ["SHELL"]
 """
+
+# packages
+#   snap => /snap or /var/lib/snap and count the directories only
+
+#  dnf system checking => /etc/fedora|redhat|centos-release
+#   dnf => search /var/lib/rpm and look for the count(*) entries in sqlite file
+
+
+# apt system checking => /etc/debian_release
+#   apt => look for /var/lib/dpkg/status file and do something there to know packages count
+# I am going to get support for dpkg | rpm based systems only right now
+def detect_installed_packages() -> str:
+    distribution_type: str = None
+    result_string: str = ""
+
+    if os.path.exists("/etc/debian_version"):
+        # debian based system
+        distribution_type = "debian"
+
+    for os_type in (
+        "/etc/redhat-release",
+        "/etc/fedora-release",
+        "/etc/centos-release",
+    ):
+        if os.path.exists(os_type):
+            distribution_type = "redhat"
+            break
+
+    if distribution_type == "redhat":
+        # have to read the sqlite file for the count of installed packages
+        with sqlite3.connect("/var/lib/rpm/rpmdb.sqlite") as conn:
+            count_of_rpm_packges = conn.execute(
+                "Select count(*) from Packages"
+            ).fetchall()[0][0]
+            result_string += str(count_of_rpm_packges) + " (rpm)"
+
+    if distribution_type == "debian":
+        # {Status: install ok installed) written for the package name in /var/lib/dpkg/status
+        with open("/var/lib/dpkg/status") as dpkg_status_file:
+            count_of_dpkg_packages = list(
+                filter(
+                    lambda line: line.startswith("Status: install ok insatlled"),
+                    dpkg_status_file.readlines(),
+                )
+            ).__len__()
+            result_string += str(count_of_dpkg_packages) + " (dpkg)"
+
+    # checking for snap packages if installed
+    if os.path.isdir("/snap"):
+        # then there are snap packages
+        count_of_snap_packages = list(
+            filter(
+                lambda dirname: os.path.isdir(dirname) and dirname != "bin",
+                os.listdir("/snap"),
+            )
+        ).__len__()
+
+        result_string += ", " + str(count_of_snap_packages) + " (snap)"
+
+    return result_string
+
+
+if __name__ == "__main__":
+    print(detect_installed_packages())
